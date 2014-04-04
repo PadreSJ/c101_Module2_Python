6ce86aeba35aedf6b9f7d2248dcdf130d5836450
PadreSJ <padre@twit.tv>
2014-04-03 17:40:43 -0700

    Create Ep01-numbers.py

---
+++ Ep01-numbers.py
@@ -0,0 +1,9 @@
+#! usr/bin/env python
+
+print "Let's find the sum two numbers!"
+
+firstValue = raw_input("Type a number and press enter: ") # Gets user input for first number to add
+
+secondValue = raw_input("Type another number and press enter: ") # Gets user input for second number to add
+
+print "Your total is", int(firstValue) + int(secondValue) # Add the two values and prints them to the screen
