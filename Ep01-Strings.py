6d2145eb4cf7e7dfb30788317d4bdb8af0400a3a
PadreSJ <padre@twit.tv>
2014-04-03 17:41:38 -0700

    Create Ep01-Strings.py

---
+++ Ep01-Strings.py
@@ -0,0 +1,9 @@
+#! usr/bin/env python
+
+print "Tell me where you live?"
+
+state = raw_input("Which state do you live in? ") # Gets a string input from the user
+
+city = raw_input("Which city do you live in? ") # Gets another string input from the user
+
+print "You live in", city + ", " + state # Combines our strings and prints them to the screen
