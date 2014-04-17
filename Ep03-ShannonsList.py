ffc833234549198124ddf6d1094225e5501ea85d
PadreSJ <padre@twit.tv>
2014-04-17 14:45:12 -0700

    Create Ep03-ShannonsList.py

---
+++ Ep03-ShannonsList.py
@@ -0,0 +1,13 @@
+print "Who doesn't like Star Trek?! "
+
+StarTrek = ['Picard', 'Kirk', 'Janeway', 'Sisko', 'Archer'];
+
+print "My favorite Star Trek captains are, in order:", StarTrek[0], StarTrek[2], StarTrek[1], StarTrek[3], StarTrek[4]
+
+
+Series = ['The Original Series', 'The Next Generation', 'Deep Space Nine', 'Voyager', 'Enterprise'];
+
+print "My favorite Star Trek series, in order, are:", Series[1], Series[0], Series[3], Series[2], Series[4]
+
+
+raw_input("Press Enter to Exit")
