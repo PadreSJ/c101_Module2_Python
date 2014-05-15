71eacd693b1284a41dc65371ca582400d91cdf42
PadreSJ <padre@twit.tv>
2014-05-15 15:11:29 -0700

    Create Ep07-04-NonDestructiveSort.py

---
+++ Ep07-04-NonDestructiveSort.py
@@ -0,0 +1,12 @@
+podcasters = ['Leo','Lisa','Padre', 'Snubs', 'Bryan', 'OMGchad', 1234]
+print podcasters
+
+
+print "Woot, let's sort!"
+sortedpodcasters = sorted(podcasters)
+
+raw_input("Press Enter to Continue")
+
+
+print podcasters
+print sortedpodcasters
