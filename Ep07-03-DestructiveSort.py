9d9b653e4ce422059ca74715432fd46902f04b84
PadreSJ <padre@twit.tv>
2014-05-15 15:11:03 -0700

    Create Ep07-03-DestructiveSort.py

---
+++ Ep07-03-DestructiveSort.py
@@ -0,0 +1,11 @@
+SortMe = [1, 23, 6, 4, 99]
+
+for entries in range (0,5):
+    print SortMe[entries]
+
+raw_input ("Press Enter")
+    
+SortMe.sort();
+
+for entries in range (0,5):
+    print SortMe[entries]
