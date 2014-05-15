12c8c96653c7b3f572f32323e7d89858e3304dcc
PadreSJ <padre@twit.tv>
2014-05-15 15:13:00 -0700

    Create Ep07-06-TupleBasics.py

---
+++ Ep07-06-TupleBasics.py
@@ -0,0 +1,13 @@
+tuppy1 = (1, 2, 3, 4, 5, 6)
+tuppy2 = ('slap', 'jump', 'cry', 'whine')
+tuppy3 = "abc", "def", "ghi", "jkl";
+
+print tuppy1
+
+raw_input("Press Enter to continue")
+
+print tuppy2
+
+raw_input("Press Enter to continue")
+
+print tuppy3
